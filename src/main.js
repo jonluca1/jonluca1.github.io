5dfe996404efe9e7b3d9d9f6622562fd6fb70d14
jonluca1 <157160424+jonluca1@users.noreply.github.com>
2024-01-21 02:06:27 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,44 +18,6 @@ console.log(" ╚══╝╚══╝ ╚═╝╚══════╝╚═�
 console.log("---------------------------------------------");
 console.log("");
 
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-   const toggle = document.getElementById(toggleId),
-         nav = document.getElementById(navId)
-
-   toggle.addEventListener('click', () =>{
-       // Add show-menu class to nav menu
-       nav.classList.toggle('show-menu')
-
-       // Add show-icon to show and hide the menu icon
-       toggle.classList.toggle('show-icon')
-   })
-}
-
-showMenu('nav-toggle','nav-menu')
-window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    if (scroll === undefined) {
-        scrollypx = "0";
-    } else {
-        scrollypx = scroll;
-    }
-
-    /*
-    var windowHeight = window.innerHeight;
-
-    scrollheight = ($(document).height() - windowHeight);
-    scrollpercent = Math.round(100 * (scrollypx / scrollheight));
-    */
-
-    scrollheight = ($(document).height() - $(window).height());
-    scrollpercent = Math.round(100 * (scrollypx / scrollheight));
-
-
-});
-scrollypx = "0";
-scrollygheight = "0";
-scrollpercent = "0";
 
 
 
